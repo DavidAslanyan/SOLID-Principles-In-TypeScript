da7287a34ae61edad0fe0f1d02bc4aee85068b08
DavidAslanyan <david.aslanyan@icketi.com>
2025-01-30 21:18:46 +0400

    Experimenting with dependency inversion principle

---
+++ dependency-inversion.ts
@@ -0,0 +1,32 @@
+interface INotificationRepository {
+  sendNotification(): void;
+}
+
+class SMSNotficationProcessor implements INotificationRepository {
+  sendNotification() {
+    console.log('SMS sent')
+  }
+}
+
+class EmailNotficationProcessor implements INotificationRepository {
+  sendNotification() {
+    console.log('Email sent')
+  }
+}
+
+
+class NotficationProcessor {
+  notificationProvider: INotificationRepository
+  constructor(notificationProvider: INotificationRepository) {
+    this.notificationProvider = notificationProvider
+  } 
+
+  sendNotification() {
+    this.notificationProvider.sendNotification()
+  }
+}
+
+const smsProvider = new SMSNotficationProcessor()
+const notificationProcessor = new NotficationProcessor(smsProvider);
+
+notificationProcessor.sendNotification()
